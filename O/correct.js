48e34bb704812bc869c97f13626015ea00833b4c
Mohamed Abdelnaeem <mohamed.abdelnaeem@widebot.net>
2020-08-04 06:08:32 +0200

    Uncomment open closed imp. in JS - Remove open closed imp. in TS until fix

--- O/correct.js
+++ O/correct.js
@@ -1,67 +1,67 @@
 
-// class BooleanQuestion {
-//   constructor(description) {
-//     this.description = description;
-//   }
+class BooleanQuestion {
+  constructor(description) {
+    this.description = description;
+  }
 
-//   printChoices() {
-//     console.log('1. True');
-//     console.log('3. False');
-//   }
-// }
+  printChoices() {
+    console.log('1. True');
+    console.log('3. False');
+  }
+}
 
-// class MultiChoiceQuestion {
-//   constructor(description, options) {
-//     this.description = description;
-//     this.options = options;
-//   }
+class MultiChoiceQuestion {
+  constructor(description, options) {
+    this.description = description;
+    this.options = options;
+  }
 
-//   printChoices() {
-//     this.options.forEach((option, index) => {
-//       console.log(`${index + 1}. ${option}`);
-//     });
-//   }
-// }
+  printChoices() {
+    this.options.forEach((option, index) => {
+      console.log(`${index + 1}. ${option}`);
+    });
+  }
+}
 
-// class TextQuestion {
-//   constructor(description) {
-//     this.description = description;
-//   }
+class TextQuestion {
+  constructor(description) {
+    this.description = description;
+  }
 
-//   printChoices() {
-//     console.log('Answer: __________________');
-//   }
-// }
+  printChoices() {
+    console.log('Answer: __________________');
+  }
+}
 
-// class RangeQuestion {
-//   constructor(description) {
-//     this.description = description;
-//   }
+class RangeQuestion {
+  constructor(description) {
+    this.description = description;
+  }
 
-//   printChoices() {
-//     console.log('Minimum: __________________');
-//     console.log('Maximum: __________________');
-//   }
-// }
+  printChoices() {
+    console.log('Minimum: __________________');
+    console.log('Maximum: __________________');
+  }
+}
 
-// function printAllQuestions(questions) {
-//   questions.forEach(question => {
-//     question.printDescription();
-//     console.log(question.description)
-//     question.printChoices();
-//     // print new line between each question
-//     console.log('');
-//   });
-// }
+function printAllQuestions(questions) {
+  questions.forEach(question => {
+    question.printDescription();
+    console.log(question.description)
+    question.printChoices();
+    // print new line between each question
+    console.log('');
+  });
+}
 
-// const Questions = [
-//   new BooleanQuestion('This is a true/false question'),
-//   new MultiChoiceQuestion(
-//     'What is your favourite language?',
-//     ['HTML', 'CSS', 'JS', 'Python']
-//   ),
-//   new TextQuestion('Describe your favourite JS feature'),
-//   new RangeQuestion('What is the speed limit in your city?')
-// ];
+const Questions = [
+  new BooleanQuestion('This is a true/false question'),
+  new MultiChoiceQuestion(
+    'What is your favourite language?',
+    ['HTML', 'CSS', 'JS', 'Python']
+  ),
+  new TextQuestion('Describe your favourite JS feature'),
+  new RangeQuestion('What is the speed limit in your city?')
+];
 
-// printAllQuestions(Questions);
+printAllQuestions(Questions);
